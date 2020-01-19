7407870fb7f643dcc15bfaab1a8d5fcba507f197
kishorgec <kishoru8@gmail.com>
2020-01-19 19:10:03 +0530

    Modify the controller name

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,13 @@ const request = require('request');
 const router = express.Router();
 const GetStarRepositoryController = require('../controller/get_repository_controller')
 
-router.get('/repos', function(req, res, next) {
+router.get('/repos', function (req, res, next) {
   res.send('Invalid API. Please use POST /repos method');
 });
 
 /* Resolving the success request */
-router.post('/repos', function(req, res, next) {
-  GetStarRepositoryController.function(req, res);
+router.post('/repos', function (req, res, next) {
+  GetStarRepositoryController.getTopStarRepoCount(req, res);
 });
 
 module.exports = router;
